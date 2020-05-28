06a8fb145f0da1d300d290ca869b134b21d8bf52
NaPiZip <napizip@mail.com>
2020-05-28 17:12:16 +0200

    Added example live production test.

--- tests/deployed_server/test_deployed.py
+++ tests/deployed_server/test_deployed.py
@@ -6,8 +6,9 @@ import pytest
 from tests.unit import helper
 
 def test_sending_example_image(mocker):
+    headers = {'x-api-key':'dummy'}
     data = helper.read_json_from_file('tests/local_host/resources/example.json')
-    response = requests.post('http://127.0.0.1:3000/v1/ocr',data=data)
+    response = requests.post('https://v1l6ko3n29.execute-api.eu-central-1.amazonaws.com/default/ocr',data=data, headers=headers)
 
     assert(response.status_code == requests.codes.ok)
     assert(response.content == b'{"result": "G Tesseract OCR\\n\\f"}')
